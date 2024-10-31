5c78b390bf433e222a91a873508979226fcde754
Mirko <ruhlmirkojoel@gmail.com>
2024-10-31 00:20:57 -0300

    Added email functionality

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,21 @@
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact Me')
 
 with st.form(key='email_form'):
     user_email = st.text_input('Your email address')
-    message = st.text_area('Your message here')
+    raw_message = st.text_area('Your message here')
+    message = f"""\
+    Subject: New Email from {user_email}
+    From: {user_email}\n    
+    {raw_message}
+    """
+    submit_btn = st.form_submit_button('Send email')
 
-    submit_btn = st.form_submit_button()
     if submit_btn:
+        st.write(raw_message)
+        st.write(message)
 
-        st.write("Thank you for your cooperation")
+        send_email(message)
+        st.info("Email sent")
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    context = ssl.create_default_context()
+
+    username = "ruhlmirkojoel@gmail.com"
+    password = "esem sezh vklb sokl"
+
+    receiver = "mirkaapo.709@gmail.com"
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
