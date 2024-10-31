538cbb3d6802963eb6d6665569b44c1daae385b9
Mirko <ruhlmirkojoel@gmail.com>
2024-10-30 22:33:53 -0300

    Rendered the app titles into two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -17,3 +18,22 @@ with col2:
     bootstrap 5, css, tailwind, ttkbootstrap, tkinter and pysimple.   
     """
     st.info(content)
+
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me
+"""
+st.write(content2)
+
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df.iterrows():
+        if index % 2 == 0:
+            st.subheader(row['title'])
+
+with col4:
+    for index, row in df.iterrows():
+        if index % 2 != 0:
+            st.subheader(row['title'])
