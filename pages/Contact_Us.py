c5c05f75c3a665ee5d3b80c30a4148b640c7e0b6
Mirko <ruhlmirkojoel@gmail.com>
2024-10-31 10:21:24 -0300

    Added topics selection menu

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit as st
 from send_email import send_email
 
@@ -5,17 +6,18 @@ st.header('Contact Me')
 
 with st.form(key='email_form'):
     user_email = st.text_input('Your email address')
+
+    df = pandas.read_csv('topics.csv')
+    topics_list = []
+    for index, row in df.iterrows():
+        topics_list.append(row['topic'])
+    selection = st.selectbox("What topic do you want to discuss", topics_list)
+
     raw_message = st.text_area('Your message here')
-    message = f"""\
-    Subject: New Email from {user_email}
-    From: {user_email}\n    
-    {raw_message}
-    """
+
+    message = f"Subject: New email from localhost\n\nFrom: {user_email} \nTopic: {selection}\n" + raw_message
     submit_btn = st.form_submit_button('Send email')
 
     if submit_btn:
-        st.write(raw_message)
-        st.write(message)
-
         send_email(message)
         st.info("Email sent")
